36e64c2bea530c86f2f8c92bed8b00b806461270
kcwiltz3244 <kcwiltz@gmail.com>
2026-07-13 13:03:45 -0500

    Update apiUrl in config.js to new endpoint

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CLASSCONNECT_CONFIG = {
-  apiUrl: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE"
+  apiUrl: "https://script.google.com/macros/s/AKfycbx7ogiaIpVUZJlYQ2f6CGuovw-04hugZ3zkKr-ND905lRcF73wDgT1a_DFndTIORkbA/exec"
 };
