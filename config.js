45a0faef8f034ce5f350249e58df3d7cbe3d85ba
kcwiltz3244 <kcwiltz@gmail.com>
2026-07-14 16:13:44 -0500

    Update API URL in CLASSCONNECT_CONFIG

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CLASSCONNECT_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbx7ogialpVUZJIYQ2f6CGuovw-04hugZ3zkKr-ND905IRcF73wDgT1a_DFndTlORkbA/exec"
+  apiUrl: "https://script.google.com/macros/s/AKfycbx7ogiaIpVUZJlYQ2f6CGuovw-04hugZ3zkKr-ND905lRcF73wDgT1a_DFndTIORkbA/exec"
 };
