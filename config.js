59358adb146f207a6bf0e7d748f013b62d46a7d4
kcwiltz3244 <kcwiltz@gmail.com>
2026-07-14 05:50:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CLASSCONNECT_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbx.../exec"
+  apiUrl: "https://script.google.com/macros/s/AKfycbx7ogiaIpVUZJlYQ2f6CGuovw-04hugZ3zkKr-ND905lRcF73wDgT1a_DFndTIORkbA/exec"
 };
