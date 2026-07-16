ae74bd86bdfea5a6c5aef28f3b2fe5dc952c7eca
kcwiltz3244 <kcwiltz@gmail.com>
2026-07-16 14:16:52 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,8 @@
-const CACHE = "classconnect-v2.5.1-phonefix-2";
-const FILES = [
+const CACHE = "classconnect-v2.5.1-datafix-1";
+const STATIC_FILES = [
   "./",
   "./index.html",
   "./styles.css",
-  "./app.js",
-  "./config.js",
   "./manifest.webmanifest",
   "./classconnect-logo.png",
   "./icon-192.png",
@@ -13,33 +11,40 @@ const FILES = [
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(FILES))
-  );
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC_FILES)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
-      )
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  const requestUrl = new URL(event.request.url);
+
+  if (
+    requestUrl.origin !== self.location.origin ||
+    requestUrl.pathname.endsWith("/config.js") ||
+    requestUrl.pathname.endsWith("/app.js")
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
   if (event.request.method !== "GET") return;
 
   event.respondWith(
     fetch(event.request)
       .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        if (response && response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        }
         return response;
       })
       .catch(() => caches.match(event.request))
